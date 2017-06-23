a7993b4a1a55ba52718c45f63a736ae33df90e86
Justin Sprigg <justin.sprigg@gmail.com>
2017-06-23 12:48:18 +1000

    Fixed data and data.previous being mixed up.

--- src/classes/delta-snapshot.js
+++ src/classes/delta-snapshot.js
@@ -1,14 +1,14 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-module.exports = function snapshotMaker(data, delta, path) {
+module.exports = function snapshotMaker(newData, oldData, path) {
   let app = admin.app();
 
   const snapshot = new functions.database.DeltaSnapshot(
     app,
     app,
-    data,
-    delta,
+    oldData,
+    newData,
     path
   );
 
