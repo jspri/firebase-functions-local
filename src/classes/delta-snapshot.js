e1ff85a2a1e780ee56e9e5574786c777f3534448
Justin Sprigg <justin.sprigg@first-rung.com>
2018-02-01 14:05:21 +1100

    Handle a bug arising from passing undefined instead of null to deltasnapshots. Caused triggers to fire at strange times and data to be lost.

--- src/classes/delta-snapshot.js
+++ src/classes/delta-snapshot.js
@@ -4,6 +4,10 @@ const functions = require('firebase-functions');
 module.exports = function snapshotMaker(newData, oldData, path) {
   let app = admin.app();
 
+  if (newData === undefined) {
+    newData = null;
+  }
+
   const snapshot = new functions.database.DeltaSnapshot(
     app,
     app,
